e5027f301ae0a41f0a0cef70e1e4736276b05ba8
Edson dos santos Toledo Junior <edson.junior@effico.com.br>
2024-10-08 12:42:32 -0300

    Preparação para deploy no Heroku

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, url_for, session, request
+
+app = Flask(__name__)
+app.secret_key = 'segredo'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastro_dados', methods=['GET', 'POST'])
+def cadastro_dados():
+    if request.method == 'POST':
+        session['nome'] = request.form['nome']
+        session['email'] = request.form['email']
+        session['endereco'] = request.form['endereco']
+        
+        session['materiais'] = []  # Inicializar a lista de materiais
+
+        return redirect(url_for('cadastro_material'))
+
+    return render_template('cadastro_dados.html')
+
+@app.route('/cadastro_material', methods=['GET', 'POST'])
+def cadastro_material():
+    if 'materiais' not in session:
+        session['materiais'] = []
+
+    if request.method == 'POST':
+        tipo = request.form['material']
+        peso = float(request.form['peso'])
+        session['materiais'].append({'tipo': tipo, 'peso': peso})
+        
+        return redirect(url_for('recompensas'))
+
+    return render_template('cadastro_material.html')
+
+@app.route('/recompensas')
+def recompensas():
+    materiais = session.get('materiais', [])
+    total_pontos = calcular_pontos(materiais)
+
+    if total_pontos is None:
+        total_pontos = 0
+
+    recompensas_disponiveis = [
+        {'nome': '1h de estacionamento', 'pontos': 150},
+        {'nome': 'Ingresso de cinema', 'pontos': 300},
+        {'nome': 'Entrada para o Inhotim', 'pontos': 700}
+    ]
+
+    recompensas_obtidas = [r['nome'] for r in recompensas_disponiveis if total_pontos >= r['pontos']]
+    
+    return render_template('recompensas.html', total_pontos=total_pontos, recompensas_obtidas=recompensas_obtidas)
+
+def calcular_pontos(materiais):
+    total_pontos = 0
+    for material in materiais:
+        if material['tipo'] == 'metal':
+            total_pontos += material['peso'] * 0.005  # 5 pts por grama de metal
+        elif material['tipo'] == 'plastico':
+            total_pontos += material['peso'] * 0.001  # 1 pt por grama de plástico
+        elif material['tipo'] == 'papel':
+            total_pontos += material['peso'] * 0.01   # 10 pts por grama de papel
+        elif material['tipo'] == 'vidro':
+            total_pontos += material['peso'] * 0.003  # 3 pts por grama de vidro
+            
+    return total_pontos
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  # Para o Heroku
